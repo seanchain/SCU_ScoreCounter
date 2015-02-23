07a300df98cdc70f4942c9fe5f3075b3ce9b53c1
Chen Sihang <seanchain2.0@gmail.com>
2015-02-24 02:06:01 +0800

    use sha1 encode to ensure the privacy and delete the html file when generating the pdf file

--- jwc/savePage.php
+++ jwc/savePage.php
@@ -6,15 +6,6 @@ function convert($filename, $zjh)
     $url = "www.chensihang.com/data/".$zjh.".html";
     $filename="../data/".$filename;
     exec("/usr/local/bin/wkhtmltopdf $url $filename");
-/*    
-    header('Content-Type: application/pdf');
-    header('Pragma: public');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header('Content-Transfer-Encoding: binary');
-    header('Content-Length: '.filesize($filename));
-    readfile("$filename");
- */
 }
 
 
@@ -29,9 +20,10 @@ fclose($file);
 echo "<meta charset=\"utf-8\">";
 echo "<h1>正在生成您的PDF文件，请稍等</h1>";
 
-$fn = $zjh."_score.pdf";
+$fn = sha1($zjh)."_score.pdf";
 
 convert($fn, $zjh);
+unlink($filesaveDir); //此时删除掉html文件
 
 echo '<script>window.location.href="http://www.chensihang.com/data/'.$fn.'"</script>';
 
